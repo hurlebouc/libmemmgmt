a45651a8c0f36a605c72b884cd61a0dfdf2dc797
hurlebouc <mathsmac@yahoo.fr>
2012-06-23 23:02:51 +0200

    Ajout de fichiers.

---
+++ MM_base.c
@@ -0,0 +1,11 @@
+//
+//  MM_base.c
+//  libmemmgmt
+//
+//  Created by Hubert Godfroy on 23/06/12.
+//  Copyright (c) 2012 Mines de Nancy. All rights reserved.
+//
+
+#include <stdio.h>
+#include <Block.h>
+#include "MM_base.h"
---
+++ MM_base.h
@@ -0,0 +1,13 @@
+//
+//  base.h
+//  libmemmgmt
+//
+//  Created by Hubert Godfroy on 23/06/12.
+//  Copyright (c) 2012 Mines de Nancy. All rights reserved.
+//
+
+#ifndef libmemmgmt_base_h
+#define libmemmgmt_base_h
+
+
+#endif
